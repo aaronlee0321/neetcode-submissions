d473866092116cc3f0693058fff2aae59ed4d365
aaronlee0321 <83861851+aaronlee0321@users.noreply.github.com>
2026-09-19 01:33:18 +1000

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        buy_idx = 0
+        sell_idx = 0
+        print(prices[buy_idx],prices[sell_idx])
+        
+        for i in range(len(prices)):
+            if prices[i] < prices[buy_idx]:
+                buy_idx = i
+                sell_idx = i
+                max_profit = max(max_profit,prices[sell_idx] - prices[buy_idx])
+            
+            elif prices[i] > prices[sell_idx]:
+                sell_idx = i
+                max_profit = max(max_profit,prices[sell_idx] - prices[buy_idx])
+
+        return max_profit
